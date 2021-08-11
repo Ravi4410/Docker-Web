0a69f62df1d0da1291523396080ad31ea215a530
Ravi4410 <85069242+Ravi4410@users.noreply.github.com>
2021-08-11 18:45:09 +0530

    Update docjs.py

--- docjs.py
+++ docjs.py
@@ -1,1 +1,26 @@
+#!/usr/bin/python3
+
+print("content-type:text/plain")
+print()
+
+import subprocess as sp
+import cgi
+
+form=cgi.FieldStorage()
+command=form.getvalue("cmd")
+output=sp.getoutput("sudo "+command)
+print(output)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
